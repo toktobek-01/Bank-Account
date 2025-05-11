c35f01a2563ceaff39b181e498bbeba22df46167
toktobek-01 <toktobek.d@gmail.com>
2025-05-11 14:46:28 -0600

    account encap

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import javax.security.auth.login.AccountException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+}
