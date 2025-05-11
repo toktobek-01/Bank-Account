75d4e8feb72b4048e678af605d2678ae8d255b12
toktobek-01 <toktobek.d@gmail.com>
2025-05-11 14:46:19 -0600

    account encap

---
+++ src/bank/account.java
@@ -0,0 +1,17 @@
+package bank;
+
+public class account {
+    public double balance;
+
+    public account(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        this.balance -= amount;
+    }
+}
